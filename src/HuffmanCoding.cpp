376c6389d17db72ba92e83c2c3c659f75987b388
Fingsinz <fingsinz@outlook.com>
2024-06-18 16:15:00 +0800

    updated 哈夫曼编码树

---
+++ src/HuffmanCoding.cpp
@@ -0,0 +1,103 @@
+﻿#include "HuffmanCoding.h"
+
+HuffmanCoding::~HuffmanCoding()
+{
+	destoryTree(root);
+}
+
+void HuffmanCoding::destoryTree(HuffmanNode *node)
+{
+	if (node == nullptr)
+		return;
+
+	destoryTree(node->left);
+	destoryTree(node->right);
+	delete node;
+}
+
+void HuffmanCoding::coding(std::vector<unsigned char> const &pixels)
+{
+	buildHuffmanTree(pixels);
+	generateCodes(root, "");
+}
+
+void HuffmanCoding::buildHuffmanTree(std::vector<unsigned char> const &pixels)
+{
+	std::unordered_map<unsigned char, int> freq;
+
+	for (unsigned char pixel : pixels)
+		freq[pixel]++;
+
+	std::priority_queue < HuffmanNode *, std::vector<HuffmanNode *>, HuffmanNodeComparator > pq;
+
+	// 排序
+	for (auto &it : freq)
+	{
+		pq.push(new HuffmanNode(it.first, it.second));
+	}
+
+	// 构建哈夫曼树
+	while (pq.size() > 1)
+	{
+		HuffmanNode *left = pq.top();
+		pq.pop();
+		HuffmanNode *right = pq.top();
+		pq.pop();
+		pq.push(new HuffmanNode(-1, left->frequency + right->frequency, left, right));
+	}
+
+	root = pq.top();
+}
+
+void HuffmanCoding::generateCodes(HuffmanNode *node, std::string code)
+{
+	if (root == nullptr)
+	{
+		return;
+	}
+
+	if (node->pixel != -1)
+	{
+		codes[node->pixel] = code;
+	}
+
+	generateCodes(node->left, code + "0");
+	generateCodes(node->right, code + "1");
+}
+
+std::string HuffmanCoding::getHuffmanCode(unsigned char pixel)
+{
+	return codes[pixel];
+}
+
+std::vector<unsigned char> HuffmanCoding::decode(std::string const &encodeData)
+{
+	std::vector<unsigned char> decodedPixels;
+	HuffmanNode *current = root;
+
+	for (char bit : encodeData)
+	{
+		if (bit == '0' and current->left != nullptr)
+		{
+			current = current->left;
+		}
+
+		else if (bit == '1' and current->right != nullptr)
+		{
+			current = current->right;
+		}
+
+		else
+		{
+			return {};
+		}
+
+		if (current->left == nullptr and current->right == nullptr)
+		{
+			decodedPixels.push_back(current->pixel);
+			current = root;
+		}
+	}
+
+	return decodedPixels;
+}
---
+++ src/HuffmanCoding.h
@@ -0,0 +1,74 @@
+﻿#pragma once
+
+#include <memory>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class HuffmanCoding
+{
+private:
+	struct HuffmanNode
+	{
+		unsigned char pixel;
+		int frequency;
+		HuffmanNode *left;
+		HuffmanNode *right;
+
+		HuffmanNode(unsigned char p, int f, HuffmanNode *l = nullptr, HuffmanNode *r = nullptr) : pixel(p), frequency(f), left(l), right(r) {}
+	};
+
+	struct HuffmanNodeComparator
+	{
+		bool operator() (HuffmanNode *l, HuffmanNode *r)
+		{
+			return l->frequency > r->frequency;
+		}
+	};
+
+private:
+	HuffmanNode *root;
+	std::unordered_map<unsigned char, std::string> codes;
+
+private:
+	void destoryTree(HuffmanNode *node);
+
+public:
+	HuffmanCoding() = default;
+
+	~HuffmanCoding();
+
+	/**
+	 * @brief 对给定的像素执行编码操作
+	 * @param pixels 像素数组
+	 */
+	void coding(std::vector<unsigned char> const &pixels);
+
+	/**
+	 * @brief 基于给定像素构建哈夫曼树
+	 * @param pixels 像素数组
+	 */
+	void buildHuffmanTree(std::vector<unsigned char> const &pixels);
+
+	/**
+	 * @brief 为给定的哈夫曼节点生成哈夫曼代码
+	 * @param node 哈夫曼树根节点
+	 * @param code 哈夫曼编码
+	 */
+	void generateCodes(HuffmanNode *node, std::string code);
+
+	/**
+	 * @brief 获取特定像素的哈夫曼代码
+	 * @param pixel 特定像素
+	 * @return String 哈夫曼编码
+	 */
+	std::string getHuffmanCode(unsigned char pixel);
+
+	/**
+	 * @brief 使用哈夫曼编码对编码数据进行解码
+	 * @param encodeData 包含编码数据的字符串
+	 * @return Vector 解码数组
+	 */
+	std::vector<unsigned char> decode(std::string const &encodeData);
+};
